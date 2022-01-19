199964eec3fc3d780c1e01a3f6e07a3848b5ebfd
burakteke <burakteke37@gmail.com>
2022-01-19 19:25:24 +0300

    New Types, Repo Layer and Seed Data

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repo.Data;
@@ -9,22 +10,34 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController: ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductController(StoreContext context)
+        public IProductRepository _repo { get; }
+        public ProductController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductAsync();
+            return Ok(products);
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<List<Product>>> GetProductTypes()
+        {
+            var products = await _repo.GetProductTypeAsync();
+            return Ok(products);
+        }
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<Product>>> GetProductBrands()
+        {
+            var products = await _repo.GetProductBrandAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
